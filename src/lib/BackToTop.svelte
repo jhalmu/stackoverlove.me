47938dce97490e5e3ab336715e7af7d49ff2992c
Juha Halmu <jhalmu@users.noreply.github.com>
2022-07-31 00:04:49 +0300

    Added scroll, tested hero, fixed typos

---
+++ src/lib/BackToTop.svelte
@@ -0,0 +1,45 @@
+<script>
+	export let showOnPx = 150;
+	let hidden = true;
+
+	function goTop() {
+		document.body.scrollIntoView();
+	}
+
+	function scrollContainer() {
+		return document.documentElement || document.body;
+	}
+
+	function handleOnScroll() {
+		if (!scrollContainer()) {
+			return;
+		}
+
+		if (scrollContainer().scrollTop > showOnPx) {
+			hidden = false;
+		} else {
+			hidden = true;
+		}
+	}
+</script>
+
+<svelte:window on:scroll={handleOnScroll} />
+
+<div class="back-to-top btn btn-primary" on:click={goTop} class:hidden>Takaisin ylös</div>
+
+<style>
+	.back-to-top {
+		opacity: 1;
+		transition: opacity 1s, visibility 1s;
+		position: fixed;
+		z-index: 99;
+		right: 20px;
+		user-select: none;
+		bottom: 20px;
+	}
+
+	.back-to-top.hidden {
+		opacity: 0;
+		visibility: hidden;
+	}
+</style>
--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,14 +1,43 @@
-<div id="top"
+<div
+	id="top"
 	class="mx-auto -my-10 navbar h-40 top-0 border-base-300 text-neutral-content divider pl-10 pr-10 pt-0 bg-white"
 >
 	<div class="mx-auto max-w-4xl">
+		<div class="navbar-start">
+			<div class="dropdown">
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label tabindex="0" class="btn btn-ghost btn-square">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h7"
+						/></svg
+					>
+				</label>
+				<ul
+					tabindex="0"
+					class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+				>
+					<li><a href="/">Homepage</a></li>
+					<li><a href="#projects">Projektit</a></li>
+					<li><a href="#about">About</a></li>
+				</ul>
+			</div>
+		</div>
 		<div class="px-2 mx-2 navbar-start min-w-fit">
 			<div class="text-lg font-bold wrap h-5">
 				Stackover<span class="bg-accent p-1 ml-1">love</span>.me
 			</div>
 			<sub class="font-thin flex-wrap text-xs">Koodausta kaninkolossa</sub>
 		</div>
-		<div class="px-2 mx-2 navbar-center">
+		<!-- 		<div class="px-2 mx-2 navbar-center">
 			<div class="flex items-stretch">
 				<a class="btn btn-ghost btn-sm" href="/">Home</a>
 
@@ -17,6 +46,6 @@
 		</div>
 		<div class="navbar-end">
 			<a class="btn btn-ghost btn-sm" href="#about">About</a>
-		</div>
+		</div> -->
 	</div>
 </div>
