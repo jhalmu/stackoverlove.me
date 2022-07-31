e4693a5100a632f0add632d2a2acafe010b9ba23
Juha Halmu <jhalmu@users.noreply.github.com>
2022-07-31 08:45:09 +0300

    typos etc

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -8,25 +8,25 @@
 			<p>
 				Opiskelen tällä hetkellä Taitotalossa ohjelmointia pääkielenä Python. Kurssin
 				pohjatarkoituksena on saada junior software developer -taso. Laajempi
-				teoreettis-käytänöllinen oppiminen tapahtuu luonnollisesti itseopiskeluna tekemällä
+				teoreettis-käytännöllinen oppiminen tapahtuu luonnollisesti itseopiskeluna tekemällä
 				harjoitteita ja omia projekteja. Sekä kurssiin kuuluvan työharjoittelun kautta.
 			</p>
 			<p>
-				Pohjakoulutuksena olen YO, ammatillisia papereita on ohjelmoija-datanomi ja mt-lähihoitaja.
+				Pohjakoulutuksena olen Yo, ammatillisia papereita on ohjelmoija-datanomi ja mt-lähihoitaja.
 				Kummastakin on työkokemusta. Aloitin ensin ohjelmoinnilla, mutta kun en ollut vielä löytänyt
 				esim Pythonia, ei auttanut kuin vaihtaa vähäksi aikaa alaa. Olen vuosien varrella
 				lämmitellyt ohjelmointitaitojani ja katsonut ja pohtinut tuhansia videoita ja useita
-				kursseaja, mutta varsinaiset projektit jäivät kolmivuorotyön himmennyksessä. Otin sivuston
+				kursseja, mutta varsinaiset projektit jäivät kolmivuorotyön himmennyksessä. Otin sivuston
 				teemaksi käsitteen Rabbid Hole, koska varsinaisen projektien puute työn ohessa pistää
 				tutustumaan kaikkeen uuteen, vähän liikaakin.
 			</p>
 			<p>
-				Harrastan ja olen harrastanut: valokuvaus, sukututkimus, sijoittaminen, voimailu ja
+				Harrastan ja olen harrastanut: valokuvaus, sukututkimus, sijoittaminen, runous, voimailu ja
 				opiskelu.
 			</p>
 			<p>
 				Ota yhteyttä esim LinkedIn:in kautta tai laita postia etunimi.sukunimi @iki.fi niin kerron
-				mielellään lisää ja katsotaan miten tarpeemme kohtaavat. Sposti löytyy qr-koodin takaa.
+				mielelläni lisää ja katsotaan miten tarpeemme kohtaavat. Sposti löytyy innovatiivisemmin qr-koodin takaa.
 			</p>
 			<p>
 				Avainsanat: frontend, backend, python, javascript, php, macos, linux, win, tailwindcss, css,
@@ -37,8 +37,8 @@
 				olekaan kuin overkill.
 			</p>
 			<p>
-				Yritän välttää puhumasta mitä aion tehdä. Sen verran sanon, että nämä sivut ovat kehityksen
-				alla.
+				Yritän välttää puhumasta mitä aion tehdä koska tekmistä ja varsinkin ideoita riittää. Sen verran sanon, että nämä sivut ovat kehityksen
+				alla. Tulossa mm. SEO-ominaisuudet. Ja se blogi pitää laittaa valumaan tänne kun tykkään pohtia.
 			</p>
 		</article>
 
